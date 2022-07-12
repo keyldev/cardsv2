5fd8e3c4309b55021eaf9cd8e8d4aa97eceb6a5a
Vasiliy <wudgexyz@gmail.com>
2022-07-13 02:32:36 +0300

    Added MVVM arch; Added CardPlateView & CardPlateViewModel

---
+++ MVVM/ViewModel/CardPlateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using cardsv2.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cardsv2.MVVM.ViewModel
+{
+    internal class CardPlateViewModel : ObservableObject
+    {
+    }
+}
