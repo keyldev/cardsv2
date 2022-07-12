f35b9131b4c853d08a8553b72652356754fc8929
Vasiliy <wudgexyz@gmail.com>
2022-07-13 02:45:27 +0300

    MVVM edits Added MainWindow VM; Added simple Plate View Design

---
+++ MVVM/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using cardsv2.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cardsv2.MVVM.ViewModel
+{
+    internal class MainWindowViewModel : ObservableObject
+    {
+        private ObservableCollection<CardPlateViewModel> _cardsList;
+
+        public ObservableCollection<CardPlateViewModel> CardsList
+        {
+            get { return _cardsList; }
+            set { _cardsList = value; NotifyPropertyChanged(); }
+        }
+
+
+
+        public MainWindowViewModel()
+        {
+            CardsList = new ObservableCollection<CardPlateViewModel>(); // init all cards list
+
+            // for test
+            CardsList.Add(new CardPlateViewModel());
+            CardsList.Add(new CardPlateViewModel());
+            CardsList.Add(new CardPlateViewModel());
+        }
+    }
+}
